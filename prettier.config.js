aee436166238a830609da1f01f8f8875e3dd55e5
dillon-todd-dev <dillontodd.dev@gmail.com>
2025-01-13 16:45:45 -0600

    remove nextjs code

--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-export default {
-  plugins: ["prettier-plugin-tailwindcss"],
-  semi: true,
-  singleQuote: true,
-  jsxSingleQuote: true,
-  tabWidth: 2,
-};
