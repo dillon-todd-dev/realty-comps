19817d0a4d6c05912d8be318e7aa6714abc33504
dillon-todd-dev <dillontodd.dev@gmail.com>
2025-01-14 05:28:06 -0600

    setup svelte app

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { Button } from "@/components/ui/button/";
+</script>
+
+<div class="flex items-center">
+    <h1 class="text-lg font-semibold md:text-2xl">Properties</h1>
+</div>
+<div class="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
+    <div class="flex flex-col items-center gap-1 text-center">
+    <h3 class="text-2xl font-bold tracking-tight">You have no properties</h3>
+    <Button class="mt-4">Add Product</Button>
+    </div>
+</div>
