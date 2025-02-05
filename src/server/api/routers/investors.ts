525c7a90214d09f4027d42a4e22f6f9d1a43ba73
dillon-todd-dev <dillontodd.dev@gmail.com>
2025-02-05 11:12:28 -0600

    add pagination to investors table

--- src/server/api/routers/investors.ts
+++ src/server/api/routers/investors.ts
@@ -4,9 +4,12 @@ import { TRPCError } from '@trpc/server';
 
 export const investorRouter = createTRPCRouter({
   getInvestors: adminProcedure
-    .input(z.object({ fromDate: z.string(), toDate: z.string() }))
+    .input(
+      z.object({ fromDate: z.string(), toDate: z.string(), page: z.number() }),
+    )
     .query(async ({ ctx, input }) => {
-      const { fromDate, toDate } = input;
+      const { fromDate, toDate, page } = input;
+      const pageSize = 20;
       const collection = ctx.mongo.collection('investors');
       try {
         const results = await collection
@@ -29,13 +32,29 @@ export const investorRouter = createTRPCRouter({
               },
             },
             {
-              $sort: { count: -1 },
+              $facet: {
+                data: [
+                  { $sort: { count: -1 } },
+                  { $skip: (page - 1) * pageSize },
+                  { $limit: pageSize },
+                ],
+                totalCount: [{ $count: 'count' }],
+              },
             },
           ])
-          .limit(20)
           .toArray();
 
-        return results;
+        if (!results || results.length === 0) {
+          return { investors: [], hasMore: false };
+        }
+
+        const investors = results[0]?.data;
+        const totalInvestors = results[0]?.totalCount[0]?.count ?? 0;
+
+        return {
+          investors,
+          hasMore: totalInvestors > page * pageSize,
+        };
       } catch (err) {
         throw new TRPCError({
           code: 'INTERNAL_SERVER_ERROR',
